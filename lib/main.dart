28085314a2d27855ab114d526e0ed4f568dc0307
xin <xin.du@ninebot.com>
2019-02-25 19:30:24 +0800

    增加多语言

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:learn_flutter/components/splash/splash_component.dart';
 import 'package:learn_flutter/widget/full_screen_widget.dart';
 
-import 'package:webview_plugin/flutter_webview.dart';
-
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: FlutterWebView(),
-      ),
+    return FullScreenComponent(
+      color: Colors.amber,
+      child: SplashComponent(),
     );
   }
 }
